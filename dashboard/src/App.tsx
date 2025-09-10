a5ffec900804ee0daa1221f231612c02d8f22631
Abhishek Sharma <abhshake17@gmail.com>
2025-09-10 21:33:34 +0530

    add custom font

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className='text-3xl'>
+      <div className='text-3xl font-poppins font-semibold'>
         But here comes Sebastian Vettel
       </div>
     </>
